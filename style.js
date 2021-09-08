aa7aec70ccf0d3fcddddcddbd36242445be6f195
shristabaruah <eternalshrista@gmail.com>
2021-09-08 13:54:09 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,40 @@
+const initialPrice = document.querySelector('#Initialprice');
+const Quantity = document.querySelector('#quantstock');
+const currentPrice = document.querySelector('#currprice');
+const outputel = document.querySelector('#output');
+const button = document.querySelector('#btn');
+
+
+function calculateProfitLoss(initial,quantity,current){
+    if(initial>current){
+        var loss = (initial - current) * quantity;
+        var lossperc = (loss/initial)*100;
+        showOutput(`The loss is ${loss} and loss percentage ${lossperc.toFixed(2)}`);
+    }else if(current > initial){
+        var profit = (current - initial) * quantity;
+        var profitperc = (profit/initial)*100;
+        showOutput(`The profit is ${profit} and profit percentage ${profitperc.toFixed(2)}`);
+        themeChange(msg);
+    }else{
+        showOutput("No pain no gain, no gain no pain");
+
+    }
+}
+
+function  submitHandler(){
+    const inl = Number(initialPrice.value);
+    const qty = Number(Quantity.value);
+    const currprice = Number(currentPrice.value);
+    calculateProfitLoss(inl,qty,currprice);
+}
+
+button.addEventListener('click',submitHandler);
+
+
+function showOutput(message){
+    outputel.innerHTML= message;
+}
+function themeChange(){
+   const msg = document.documentElement.setAttribute("data-theme","dark");
+}
+
