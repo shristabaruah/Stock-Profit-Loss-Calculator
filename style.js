3bc6da954d8976d66d317e0ffa6e69a220bc5d00
shristabaruah <eternalshrista@gmail.com>
2021-09-20 20:28:26 +0530

    change in negative no

--- style.js
+++ style.js
@@ -8,13 +8,13 @@ const button = document.querySelector('#btn');
 function calculateProfitLoss(initial,quantity,current){
     if(initial>current){
         var loss = (initial - current) * quantity;
-        var lossperc = (loss/initial)*100;
-        showOutput(`The loss is Rs${loss} and loss percentage is ${lossperc.toFixed(2)}%`);
+        var lossperc = (((initial-current)*100)/initial).toFixed(2);
+        showOutput(`The loss is Rs${loss} and loss percentage is ${lossperc}%`);
     }else if(current > initial){
         var profit = (current - initial) * quantity;
-        var profitperc = (profit/initial)*100;
-        showOutput(`The profit is Rs${profit} and profit percentage is ${profitperc.toFixed(2)}%`);
-        themeChange(msg);
+        var profitperc = (((current-initial)*100)/initial).toFixed(2);
+        showOutput(`The profit is Rs${profit} and profit percentage is ${profitperc}%`);
+    
     }else{
         showOutput("No pain no gain, no gain no pain");
 
@@ -25,7 +25,14 @@ function  submitHandler(){
     const inl = Number(initialPrice.value);
     const qty = Number(Quantity.value);
     const currprice = Number(currentPrice.value);
-    calculateProfitLoss(inl,qty,currprice);
+    
+    if ((inl > 0) && (qty > 0) && (currprice > 0) ) {
+        calculateProfitLoss(inl, qty, currprice);
+    }
+    else {
+        showOutput("Enter valid input");
+    }
+    
 }
 
 button.addEventListener('click',submitHandler);
